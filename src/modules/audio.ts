911e688461d32512a1454bebebdeda33246fb076
Csaba Csecskedi <csaba@spinplaygames.com>
2026-09-03 20:36:49 +0200

    sudio export

--- src/modules/audio.ts
+++ src/modules/audio.ts
@@ -293,3 +293,50 @@ export async function audio(bitrate = 44100, ctx = new AudioContext()) {
         FREQ.push(pow(a, n) * 440)
     }
 }
+
+/**
+ * Create download URL from buffer data
+ *
+ * @param id - The buffer ID.
+ * @returns Download URL
+ */
+export function audioToURL(id: string): string {
+    const buffer = BUFFERS.get(id)
+    if (!buffer) {
+        return ""
+    }
+    const numChannels = buffer.numberOfChannels
+    const sampleRate = buffer.sampleRate
+    const length = buffer.length * numChannels * 2
+    const arrayBuffer = new ArrayBuffer(44 + length)
+    const view = new DataView(arrayBuffer)
+    const writeString = (offset: number, value: string) => {
+        for (let i = 0; i < value.length; i++) {
+            view.setUint8(offset + i, value.charCodeAt(i))
+        }
+    }
+
+    writeString(0, "RIFF")
+    view.setUint32(4, 36 + length, true)
+    writeString(8, "WAVE")
+    writeString(12, "fmt ")
+    view.setUint32(16, 16, true)
+    view.setUint16(20, 1, true)
+    view.setUint16(22, numChannels, true)
+    view.setUint32(24, sampleRate, true)
+    view.setUint32(28, sampleRate * numChannels * 2, true)
+    view.setUint16(32, numChannels * 2, true)
+    view.setUint16(34, 16, true)
+    writeString(36, "data")
+    view.setUint32(40, length, true)
+
+    let offset = 44
+    for (let i = 0; i < buffer.length; i++) {
+        for (let channel = 0; channel < numChannels; channel++) {
+            const sample = Math.max(-1, Math.min(1, buffer.getChannelData(channel)[i]))
+            view.setInt16(offset, sample < 0 ? sample * 0x8000 : sample * 0x7fff, true)
+            offset += 2
+        }
+    }
+    return URL.createObjectURL(new Blob([arrayBuffer], { type: "audio/wav" }))
+}
